ce2373aafd1df01f02db9d033b8fc3a42570be2d
Richard Tunstall <ricktunstalljr@gmail.com>
2023-04-24 01:02:03 -0400

    sqlalchemy

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,13 +2,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-
 app = Flask(__name__)
 
 app.secret_key = "youreawizardharry"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db = SQLAlchemy(app)
 
-db.init_app(app)
-
-from flask_app.controllers import blogs_control
--- server.py
+++ server.py
@@ -3,4 +3,5 @@ from flask_app import app
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
